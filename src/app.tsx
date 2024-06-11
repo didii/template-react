d5d6211248c880325d7fff4e7ff3fd939d2baf2e
didii <dieter.vbroeck@gmail.com>
2024-06-11 19:04:02 +0200

    Add the famous counter button

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,17 @@
+import { useCallback, useState } from 'react';
+
 export function App() {
-  return <div>This is the app</div>;
+  const [count, setCount] = useState(0);
+  const incrementCount = useCallback(() => {
+    setCount((x) => x + 1);
+  }, []);
+
+  return (
+    <div>
+      <h1>This is the app</h1>
+      <div>
+        <button onClick={incrementCount}>Count = {count}</button>
+      </div>
+    </div>
+  );
 }
